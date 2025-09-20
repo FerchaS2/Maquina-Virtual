2c2d0736ae3fe1dbd57438930583b0af4c7188ac
FerchaS2 <ferdanros@hotmail.com>
2025-09-20 19:31:41 -0300

    Fix traductor

--- src/memory.c
+++ src/memory.c
@@ -6,7 +6,7 @@ void traductor(MV * mv, uint16_t segm, uint16_t offset, int nbytes, uint32_t *di
         mv->err = ERR_SEG;
     else {
         SegDesc seg = mv->segmentos[segm];
-        if (offset + nbytes > (seg.tam+1)) {
+        if ((offset + nbytes > seg.tam) && (offset < seg.tam)) { //si offset == seg.tam significa que es la última instrucción (STOP debería ser siempre si no se rompe)
             mv->err = ERR_FDSEG;
         } else
             *dir = seg.base + offset;
